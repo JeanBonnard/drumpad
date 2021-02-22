e6181dfe452ef8e3eeab836251008d607b716b3d
JeanBonnard <j.bonnard42@gmail.com>
2021-02-22 11:57:00 +0100

    ajout definitif

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ let keys = document.querySelectorAll('.key')
 let audios = document.querySelectorAll('audio')
 
 window.addEventListener('keydown',function (e){
-
     keys.forEach(touche=>{
+
         if(e.keyCode == touche.dataset.key){
             touche.classList.add('playing','sound')
-
+            touche.addEventListener('transitionend',function(){
+                touche.classList.remove('playing','sound')
+            })
             audios.forEach(audio=>{
                 if(e.keyCode == audio.dataset.key){
                     audio.play()
@@ -15,18 +17,84 @@ window.addEventListener('keydown',function (e){
         }
     })
 })
-window.addEventListener('keyup',function (e){
 
-    keys.forEach(touche=>{
-        if(e.keyCode == touche.dataset.key){
-            touche.classList.remove('playing','sound')
+async function beatBox() {
+    function simulateKey(number){
+        let keyboardEvent = new KeyboardEvent('keydown', {
+            keyCode : number
+        })
+        window.dispatchEvent(keyboardEvent);
+    }
 
-            audios.forEach(audio=>{
-                if(e.keyCode == audio.dataset.key){
-                    audio.pause()
-                    audio.currentTime = 0;
-                }
-            })
-        }
-    })
-})
+    function playBeat(number,time){
+        return new Promise(function (resolve,reject){
+            setTimeout(function (){
+                resolve(simulateKey(number))
+            },time)
+        })
+    }
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(83,600)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(83,600)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(83,600)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(83,600)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(83,600)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(83,600)
+    await playBeat(90,300)
+    await playBeat(69,600)
+
+    await playBeat(65,300)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(83,600)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(83,600)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(83,600)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(83,600)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(83,600)
+    await playBeat(90,300)
+    await playBeat(69,600)
+    await playBeat(65,300)
+    await playBeat(83,600)
+    await playBeat(90,300)
+    await playBeat(69,600)
+}
